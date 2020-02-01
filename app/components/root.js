af69916f145646ed6126f369c8d524c43799982c
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-02-01 18:15:45 -0500

    first

--- app/components/root.js
+++ app/components/root.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Root = () => {
   return (
     <div>
-      <nav>
-        Welcome!
-      </nav>
+      <nav>Welcome!</nav>
       <main>
-        <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
+        <h1>Welcome to the Kaitlyn Hamilton Academy of JavaScript!</h1>
         <p>This seems like a nice place to get started with some Routes!</p>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
